945e5a95eecc553777f9910bfc8c959992372c9f
samratmahmud <akashrahman.me@gmail.com>
2024-01-23 22:07:19 +0600

    features compo improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,17 @@ import Responsive from "./home/Responsive";
 import Header from "./home/header/Header";
 
 export default function Home() {
-  return (
-    <main className="pb-20">
-      <Header />
-      <PremiumFeatures />
-      <Demos />
-      <PagesGroup />
-      <MasonaryCards />
-      <Responsive />
-      <Features />
-      <Advanced />
-      <FeaturesCards />
-    </main>
-  );
+   return (
+      <main className="pb-20">
+         <Header />
+         <PremiumFeatures />
+         {/* <Demos /> */}
+         <PagesGroup />
+         <MasonaryCards />
+         <Responsive />
+         <Features />
+         <Advanced />
+         <FeaturesCards />
+      </main>
+   );
 }
